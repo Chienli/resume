537333d7bae689477694c969178cd86a3d7422f8
chienli <chienli@chienlis-MacBook-Pro.local>
2020-04-08 00:00:37 +0800

    build: add material ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Button from '@material-ui/core/Button';
+
+
+const Resume = styled.div`
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Resume />
+      <Button variant="contained" color="primary">
+      你好，世界
+    </Button>
     </div>
   );
 }
